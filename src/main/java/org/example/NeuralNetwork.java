2f9e6fe9bec611c6cf09c1b7a03b2d94cc5421da
Carlos <solrak_27@hotmail.com>
2024-08-21 21:25:41 +0200

    plantilla creada

---
+++ src/main/java/org/example/NeuralNetwork.java
@@ -0,0 +1,9 @@
+package org.example;
+
+
+
+// Contiene múltiples capas y define cómo las neuronas están conectadas.
+
+
+public class NeuralNetwork {
+}
---
+++ src/main/java/org/example/Neuron.java
@@ -0,0 +1,7 @@
+package org.example;
+
+
+//Representa una neurona con pesos de entrada y una función de activación.
+
+public class Neuron {
+}
