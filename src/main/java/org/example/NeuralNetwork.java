a8187069016331997ae1aa0affb7d57a9ea8874d
Carlos <solrak_27@hotmail.com>
2024-08-31 14:24:49 +0200

    Clases creadas y algunos metodos

--- src/main/java/org/example/NeuralNetwork.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-
-
-// Contiene múltiples capas y define cómo las neuronas están conectadas.
-
-
-public class NeuralNetwork {
-}
--- src/main/java/org/example/Neuron.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-
-//Representa una neurona con pesos de entrada y una función de activación.
-
-public class Neuron {
-}
